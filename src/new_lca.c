7e2643414abebc341413ba8701b79fe4fcf9f160
jackgilbride999 <jackgilbride999@gmail.com>
2019-10-07 19:26:24 +0100

    Tests for NULL and single node tree written and passed.

--- src/new_lca.c
+++ src/new_lca.c
@@ -10,8 +10,34 @@ struct node* new_node(int key)
 	return node; 
 } 
 
-struct node * get_lca(struct node * root, int x, int y){
-    if(root == NULL){
+struct node * get_lca_recursive(struct node * this, int x, int y){
+    int x_matched = 0;
+    int y_matched = 0;
+    if(this == NULL){
+        // end of recursion, return NULL
         return NULL;
     }
+    if(this->key == x || this->key ==y){
+        // key has been matched, return non-NULL value
+        return this;
+    }
+    struct node * lca_left = get_lca(this->left, x, y);
+    struct node * lca_right = get_lca(this->right, x, y);
+    if(lca_left && lca_right){
+        // key has been matched on both left and right subtrees, this is LCA
+        return this;
+    } else if(lca_left){
+        return lca_left;
+    } else if(lca_right){
+        return lca_right;
+    }
+}
+
+struct node * get_lca(struct node * root, int x, int y){
+    struct node * result = get_lca_recursive(root, x, y);
+    if(result==NULL || result->key == x || result ->key == y){
+        // LCA has not been found if NULL or either of the keys was returned
+        return NULL;
+    }
+    return result;
 }
--- test/Testnew_lca.c
+++ test/Testnew_lca.c
@@ -3,16 +3,66 @@
 #include "new_lca.h"
 #include "unity.h"
 #include <assert.h>
+#include <stdlib.h>
 
+/*
+    Test get_lca when the root pointer is NULL
+*/
 void test_EmptyTree(void){
-    struct node* test = get_lca(NULL, 0, 1);
-    TEST_ASSERT_NULL_MESSAGE(test, "LCA of with NULL tree was not NULL.")
+    // both ints as zero
+    struct node* lca = get_lca(NULL, 0, 0);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of NULL tree was not NULL.");
+    free(lca);
+
+    // both ints as equal, non-zero values
+    lca = get_lca(NULL, 5, 5);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of NULL tree was not NULL.");
+    free(lca);
+
+    // both ints different
+    lca = get_lca(NULL, 10, 4);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of NULL tree was not NULL.");
+    free(lca);
 }
 
 
+/*
+    Test get_lca for a single node tree. All tests should return NULL as
+    a single node is not an ancestor of any other nodes.
+*/
+void test_SingleNodeTree(void){
+    // both ints as zero
+    struct node* root = new_node(0);
+    struct node* lca = get_lca(root, 0, 0);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of single node tree was not NULL.")
+    free(root);
+    free(lca);
+
+    // both ints different, one is zero
+    root = new_node(0);
+    lca = get_lca(root, 0, 1);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of single node tree was not NULL.")
+    free(root);
+    free(lca);
+
+    // both ints different, non-zero values
+    root = new_node(5);
+    lca = get_lca(root, 10, 15);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of single node tree was not NULL.")
+    free(root);
+    free(lca);
+
+    // both ints equal, non-zero values
+    root = new_node(40);
+    lca = get_lca(root, 40, 40);
+    TEST_ASSERT_NULL_MESSAGE(lca, "LCA of single node tree was not NULL.")
+    free(root);
+    free(lca);
+}
 
 int main(void){
     UNITY_BEGIN();
     RUN_TEST(test_EmptyTree);
+    RUN_TEST(test_SingleNodeTree);
     return UNITY_END();
 }
